5e36cf80badc52d895df5c7f7b5b6798bf8db766
Joaquín Rodrigo Araya Monsalve <joa.arayam@duocuc.cl>
2025-11-25 15:21:42 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
+import sys
+import os
+
+# Asegurar que Jenkins vea el directorio actual
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from app import app
 
-def test_home():
+def test_home_route():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+    assert response.data == b"Hola desde Flask - Pipeline funcionando!"
